c5b20bc50d0f7ee5c344292cc11afc8e6036e1a7
Lucas Kramer <krame505@umn.edu>
2018-11-24 18:46:34 -0600

    Add new term construction syntax

---
+++ grammars/edu.umn.cs.melt.exts.ableC.prolog/abstractsyntax/Term.sv
@@ -0,0 +1,39 @@
+grammar edu:umn:cs:melt:exts:ableC:prolog:abstractsyntax;
+
+abstract production termExpr
+top::Expr ::= ty::TypeName allocator::Expr le::LogicExpr
+{
+  propagate substituted;
+  top.pp = pp"term<${ty.pp}>(${allocator.pp}) {${le.pp}}";
+  
+  local expectedAllocatorType::Type =
+    functionType(
+      pointerType(
+        nilQualifier(),
+        builtinType(nilQualifier(), voidType())),
+      protoFunctionType([builtinType(nilQualifier(), unsignedType(longType()))], false),
+      nilQualifier());
+  
+  local localErrors::[Message] =
+    ty.errors ++ allocator.errors ++ le.errors ++
+    (if !ty.typerep.isCompleteType(top.env)
+     then [err(top.location, s"term type parameter has incomplete type ${showType(ty.typerep)}")]
+     else []) ++
+    (if !compatibleTypes(expectedAllocatorType, allocator.typerep, true, false)
+     then [err(allocator.location, s"Allocator must have type void *(unsigned long) (got ${showType(allocator.typerep)})")]
+     else []) ++
+    checkUnificationHeaderTemplateDef("_var_d", top.location, top.env);
+  
+  le.env = addEnv(ty.defs, ty.env);
+  le.expectedType = extType(nilQualifier(), varType(ty.typerep));
+  le.allowUnificationTypes = false;
+  le.allocator = allocator;
+  
+  local fwrd::Expr =
+    ableC_Expr {
+      ({$Stmt{makeVarDecls(le.defs)}
+        $Expr{le.transform};})
+    };
+  
+  forwards to mkErrorCheck(localErrors, fwrd);
+}
---
+++ grammars/edu.umn.cs.melt.exts.ableC.prolog/concretesyntax/Term.sv
@@ -0,0 +1,7 @@
+grammar edu:umn:cs:melt:exts:ableC:prolog:concretesyntax;
+
+marking terminal Term_t 'term' lexer classes {Ckeyword};
+
+concrete productions top::PrimaryExpr_c
+| 'term' '<' ty::TypeName_c '>' LParen_t allocate::AssignExpr_c ')' '{' le::LogicExpr_c '}'
+  { top.ast = termExpr(ty.ast, allocate.ast, le.ast, location=top.location); }
