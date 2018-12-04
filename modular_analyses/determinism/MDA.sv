1e9c71dc26d0039acde7382d30963a46127532a4
Lucas Kramer <krame505@umn.edu>
2018-12-04 16:35:26 -0600

    Add list mda test

--- modular_analyses/determinism/MDA.sv
+++ modular_analyses/determinism/MDA.sv
@@ -11,3 +11,8 @@ copper_mda testCore(ablecParser) {
   edu:umn:cs:melt:exts:ableC:prolog:core:concretesyntax;
 }
 
+copper_mda testList(ablecParser) {
+  edu:umn:cs:melt:exts:ableC:prolog:core:concretesyntax;
+  edu:umn:cs:melt:exts:ableC:prolog:list:concretesyntax;
+}
+
