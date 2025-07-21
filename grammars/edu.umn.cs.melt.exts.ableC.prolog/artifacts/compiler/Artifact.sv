60822a5e82526e913a9adb8c574cc2556eed432e
Lucas Kramer <krame505@umn.edu>
2025-07-21 18:29:11 -0500

    Add codeprober driver

--- grammars/edu.umn.cs.melt.exts.ableC.prolog/artifacts/compiler/Artifact.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.prolog/artifacts/compiler/Artifact.sv
@@ -8,7 +8,9 @@ grammar edu:umn:cs:melt:exts:ableC:prolog:artifacts:compiler;
  -}
 
 import edu:umn:cs:melt:ableC:concretesyntax as cst;
+import edu:umn:cs:melt:ableC:abstractsyntax:host;
 import edu:umn:cs:melt:ableC:drivers:compile;
+import edu:umn:cs:melt:ableC:drivers:codeProber as cpr;
 
 
 parser extendedParser :: cst:Root {
@@ -18,3 +20,6 @@ parser extendedParser :: cst:Root {
 } 
 
 fun main IO<Integer> ::= args::[String] = driver(args, extendedParser);
+
+fun codeProberParse IO<Decorated Compilation> ::= args::[String] =
+  cpr:driver(args, extendedParser);
